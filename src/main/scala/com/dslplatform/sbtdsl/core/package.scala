fd94838317e3e20dc38d961f871188971956d379
Tomo Ferega <tomo@element.hr>
2016-04-08 01:34:52 +0200

    Reorganize imports in core package object

--- src/main/scala/com/dslplatform/sbtdsl/core/package.scala
+++ src/main/scala/com/dslplatform/sbtdsl/core/package.scala
@@ -1,29 +1,30 @@
 package com.dslplatform.sbtdsl
 
-import com.dslplatform.compiler.client
+import com.dslplatform.compiler.client.{ Context => ClcContext, Main => ClcMain }
+import com.dslplatform.compiler.client.{ parameters => clc }
 
 package object core {
   def compileDsl(paths: Options.Paths): Unit = {
-    val context = new client.Context()
+    val context = new ClcContext()
 
     // Basic settings
-    context.put(client.parameters.Download.INSTANCE, null)
-    context.put(client.parameters.Namespace.INSTANCE, "org.example")
-    context.put(client.parameters.PostgresConnection.INSTANCE, "localhost:5432/storage_db?user=storage_user&password=storage_pass")
+    context.put(clc.Download.INSTANCE, null)
+    context.put(clc.Namespace.INSTANCE, "org.example")
+    context.put(clc.PostgresConnection.INSTANCE, "localhost:5432/storage_db?user=storage_user&password=storage_pass")
 
     // Target settings
     context.put("revenj.java", "lib/storage-revenj.jar")
 
     // Other settings
-    context.put(client.parameters.Settings.INSTANCE, "manual-json")
+    context.put(clc.Settings.INSTANCE, "manual-json")
 
     // Paths and locations
-    context.put(client.parameters.DslPath.INSTANCE, paths.dsl)
-    context.put(client.parameters.Dependencies.INSTANCE, paths.lib)
-    context.put(client.parameters.SqlPath.INSTANCE, paths.sql)
+    context.put(clc.DslPath.INSTANCE, paths.dsl)
+    context.put(clc.Dependencies.INSTANCE, paths.lib)
+    context.put(clc.SqlPath.INSTANCE, paths.sql)
 
-    context.put(client.parameters.ApplyMigration.INSTANCE, null)
-    val params = client.Main.initializeParameters(context, ".")
-    client.Main.processContext(context, params)
+    context.put(clc.ApplyMigration.INSTANCE, null)
+    val params = ClcMain.initializeParameters(context, ".")
+    ClcMain.processContext(context, params)
   }
 }
